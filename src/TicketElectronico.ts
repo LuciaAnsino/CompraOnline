774766237edd2a0c0bb80c2b794444146b1d9830
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-10-23 13:26:30 +0200

    Cambios requeridos

--- src/TicketElectronico.ts
+++ src/TicketElectronico.ts
@@ -3,25 +3,22 @@
  */
 
 class TicketElectronico {
-    private cod_ticket: number;
+    private readonly cod_ticket: number;
     private fecha_compra: Date;
     private alimentos: Alimento[];
-    private precio_compra: number;
-    private precio_alimento: number; // Cantidad x precio unidad
+    private cantidad: number;
 
     /**
      * Constructor de la clase TicketElectronico
      * @param cod_ticket Código del ticket
      * @param fecha_compra Fecha de la compra
      * @param alimentos Alimentos comprados
-     * @param precio_compra Precio total de la compra
-     * @param precio_alimento Precio total de cada alimento
+     * @param cantidad Cantidad de cada alimento
      */
-    constructor(cod_ticket: number, fecha_compra: Date, alimentos: Alimento[], precio_compra: number, precio_alimento: number) {
+    constructor(cod_ticket: number, fecha_compra: Date, alimentos: Alimento[], cantidad: number) {
         this.cod_ticket = cod_ticket;
         this.fecha_compra = fecha_compra;
         this.alimentos = alimentos;
-        this.precio_compra = precio_compra;
-        this.precio_alimento = precio_alimento;
+        this.cantidad = cantidad;
     }
 }
